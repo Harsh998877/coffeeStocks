5ae7120d7488fa525a1d38052587336e1777bd91
Harsh Gupta <harshgupta9694@gmail.com>
2023-04-03 19:58:52 +0530

    Updated

--- couponIdPage.js
+++ couponIdPage.js
@@ -2,12 +2,25 @@ import React, { useState } from 'react';
 
 function CouponPage() {
   const [couponId, setCouponId] = useState('');
+  const [couponData, setCouponData] = useState(null);
 
   const handleSubmit = async () => {
     // Make API call here with couponId
-    
-
-
+    try {
+      const couponResp = await fetch('/your-api-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ couponId })
+      });
+      //success
+      const couponInfo = await couponResp.json();
+      setCouponData(couponInfo);
+    } catch (error) {
+      //failure
+      console.log('Invalid coupon id. Please pay to drink... chutiya nhi hai hm free mei denge')
+    }
   };
 
   return (
@@ -17,8 +30,23 @@ function CouponPage() {
         value={couponId}
         onChange={e => setCouponId(e.target.value)}
         placeholder="Enter coupon ID"
+        style={{ margin: '10px', padding: '5px' }}
       />
-      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={handleSubmit} style={{ padding: '5px 10px' }}>
+        Submit
+      </button>
+
+      {
+      
+        /*Now show coupon data*/
+        couponData && (
+          <div>
+            {/* Scratch code here */}
+          </div>
+        )
+      
+      }
+      
     </div>
   );
 }
